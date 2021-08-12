52e321b27187e38d7cf7a141af0b8e3bfbd303a6
lsw271 <54321607+lsw271@users.noreply.github.com>
2021-08-12 21:56:37 +0200

    Update sdm.go
    added more registers

--- internal/meters/sdm.go
+++ internal/meters/sdm.go
@@ -21,30 +21,16 @@ const (
 	
 	OpCodeSDMTotalSysPow     = 0x0034
 	OpCodeSDMSumCurr         = 0x0010
-	/* add another registers
-	OpCodeSDML3Power     = 0x0010
-	
-	OpCodeSDML3Power     = 0x0010
-	OpCodeSDML3Power     = 0x0010
-	OpCodeSDML3Power     = 0x0010
-	
-	OpCodeSDML3Power     = 0x0010
-	OpCodeSDML3Power     = 0x0010
-	OpCodeSDML3Power     = 0x0010
-	
-	OpCodeSDML3Power     = 0x0010
-	OpCodeSDML3Power     = 0x0010
-	OpCodeSDML3Power     = 0x0010
-	
-	OpCodeSDML3Power     = 0x0010
-	OpCodeSDML3Power     = 0x0010
-	OpCodeSDML3Power     = 0x0010
-	
-	OpCodeSDML3Power     = 0x0010
-	OpCodeSDML3Power     = 0x0010
-	OpCodeSDML3Power     = 0x0010	
-	*/
-	
+	OpCodeSDMTotalKwh        = 0x0156
+	OpCodeSDMTotalKvarh      = 0x0158
+	OpCodeSDML1_L2_v         = 0x00C8
+	OpCodeSDML2_L3_v         = 0x00CA
+	OpCodeSDML3_L1_v         = 0x00CC
+	OpCodeSDMNeuCurrent		 = 0x00E0
+	OpCodeSDML1_total_kwh    = 0x0166
+	OpCodeSDML2_total_kwh 	 = 0x0168
+	OpCodeSDML3_total_kwh 	 = 0x016a
+		
 	OpCodeSDML1Import    = 0x015a
 	OpCodeSDML2Import    = 0x015c
 	OpCodeSDML3Import    = 0x015e
@@ -103,6 +89,21 @@ func (p *SDMProducer) Produce() (res []Operation) {
 	res = append(res, p.snip(OpCodeSDML1Power, "WLocPhsA"))
 	res = append(res, p.snip(OpCodeSDML2Power, "WLocPhsB"))
 	res = append(res, p.snip(OpCodeSDML3Power, "WLocPhsC"))
+	
+	res = append(res, p.snip(OpCodeSDMTotalSysPow, "WLocPhsSUM"))
+	res = append(res, p.snip(OpCodeSDMSumCurr, "AmpLocPhsSUM"))
+	res = append(res, p.snip(OpCodeSDMTotalKwh, "TotalKwh"))
+	res = append(res, p.snip(OpCodeSDMTotalKvarh, "TotalKvarh"))
+	
+	res = append(res, p.snip(OpCodeSDML1_L2_v, "V_L1_l2"))
+	res = append(res, p.snip(OpCodeSDML2_L3_v, "V_l2_l3"))
+	res = append(res, p.snip(OpCodeSDML3_L1_v, "V_l3_l1"))
+	
+	res = append(res, p.snip(OpCodeSDMNeuCurrent, "NeuCurrent"))
+	
+	res = append(res, p.snip(OpCodeSDML1_total_kwh, "L1_TotalKwh"))
+	res = append(res, p.snip(OpCodeSDML2_total_kwh, "L2_TotalKwh"))
+	res = append(res, p.snip(OpCodeSDML3_total_kwh, "L3_TotalKwh"))
 
 	res = append(res, p.snip(OpCodeSDML1Cosphi, "AngLocPhsA"))
 	res = append(res, p.snip(OpCodeSDML2Cosphi, "AngLocPhsB"))
